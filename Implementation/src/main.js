958ba77afcbc9cc612de8fbff50d3f742a6b005c
Haran Dev Murugan <m.harandev@gmail.com>
2025-03-27 21:24:20 +0100

    Tested protective range
    General testing of protective range with logs

--- Implementation/src/main.js
+++ Implementation/src/main.js
@@ -1,12 +1,20 @@
-import { Boid } from "Boid.js";
+import { Boid } from "./Boid.js";
 
 function randomize(n)
 {
     
     let boids = [];
-    for (let i = 0; i < n; i++) {
+    
+    boids.push(new Boid(2,3,3,3));
+    Printer("Added first boid "+"Position: "+boids[boids.length-1].x+","+boids[boids.length-1].y+" With initial velocity: "+boids[boids.length-1].vx+","+boids[boids.length-1].vy); 
+    boids.push(new Boid(8,5,1,3));
+    Printer("Added first boid "+"Position: "+boids[boids.length-1].x+","+boids[boids.length-1].y+" With initial velocity: "+boids[boids.length-1].vx+","+boids[boids.length-1].vy);
+    boids.push(new Boid(5,4,3,3));
+    Printer("Added first boid "+"Position: "+boids[boids.length-1].x+","+boids[boids.length-1].y+" With initial velocity: "+boids[boids.length-1].vx+","+boids[boids.length-1].vy);  
+
+    /*for (let i = 0; i < n; i++) {
         boids.push(new Boid(Math.random()*101, Math.random()*201, Math.random()*11, Math.random()*11));
-    }  
+    } */ 
     return boids;
 } 
 
@@ -32,29 +40,48 @@ function controller()
 
     //min speed and mzx speed
     let min_speed = 1.2,
-        max_Speed = 10;
+        max_speed = 10;
 
     
 
     for (let i= 0; i < boids.length; i++)
     {
         // set accumulator variables to zero
-    let dx,dy,close_dx, close_dy, xvel, yvel, xpos_avg, ypos_avg, xvel_avg, yvel_avg,neighbours = 0;
+        let dx =0 ,
+            dy = 0,
+            close_dx = 0, 
+            close_dy = 0, 
+            xvel = 0, 
+            yvel = 0, 
+            xpos_avg = 0, 
+            ypos_avg = 0, 
+            xvel_avg = 0, 
+            yvel_avg = 0,
+            neighbours = 0;
+
+    
 
     let temp = boids[i];
-    for (let j = 0; j != i && j <boids.length; j++)
+    for (let j = 0; j <boids.length; j++)
     {
-        dx = temp.x - boid[j].x;
-        dy = temp.y - boid[j].y;
+        if(i==j) continue;
+
+        Printer("Enetered Boid Algorithm...."+ " Current boid: " + i + " Other Boid: " + j);
+        dx = temp.x - boids[j].x;
+        dy = temp.y - boids[j].y;
+        Printer("dx:"+dx+"dy:"+dy);
 
         if( Math.abs(dx) < vis_range && Math.abs(dy) < vis_range)
         {
+           
             let sq_distance = dx*dx + dy*dy;
             // check seperation 
             if(sq_distance < (protective_range*protective_range))
             {
-                close_dx = temp.x - boid[j].x;
-                close_dy = temp.y - boid[j].y;
+               
+                close_dx += temp.x - boids[j].x;
+                close_dy += temp.y - boids[j].y;
+                Printer("Seperation has been done " + "close_dx = " + close_dx + "close_dy = " + close_dy);
 
             } 
             //check alignment i cohesion 
@@ -99,19 +126,24 @@ function controller()
         temp.vx = (temp.vx/speed) * min_speed;
         temp.vy = (temp.vy/speed) * min_speed;
     }
-    if(speed > max_Speed) 
+    if(speed > max_speed) 
     {
         temp.vx = (temp.vx/speed) * max_speed;
         temp.vy = (temp.vy/speed) * max_speed;
     }
 
     //update positions of boid
-    temp.x = temp.x + temp,vx;
+    temp.x = temp.x + temp.vx;
     temp.y =  temp.y + temp.vy;
+
+    Printer("Final position of boid:"+(i+1)+":"+temp.x+","+temp.y);
     
 }
 }
 
 controller();
 
-console.log("Hellow, World");
+function Printer (s)
+{
+    console.log(s);
+}
